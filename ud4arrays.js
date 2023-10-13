8089ebceb1dbfe149aa372e8bbbbab92d0ab4e07
Albert <albertweb08@gmail.com>
2023-10-13 20:44:57 +0200

    Ejercicio Array de arrays

--- ud4arrays.js
+++ ud4arrays.js
@@ -1,1 +1,15 @@
 'use strict'
+//Que pida al usuario filas y columnas que cree una matriz. Array de arrays con número aleatorios. 
+
+let arrayExt=prompt("Longitud primer array");
+let arrayInt=prompt("Longitud arrays interiores");
+
+let matriz=[];
+
+for (let j=0;j<=(arrayExt-1);j++){
+    matriz[j]= [];
+    for (let i=0; i<=(arrayInt-1);i++){
+        matriz[j][i]=parseInt((Math.random())*100);
+        console.log("valor raiz"+matriz[j][i]);
+    } 
+}
