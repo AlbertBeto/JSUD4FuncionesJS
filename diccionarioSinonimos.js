5193876c375ddcf1e8d8eb1a15f1900ad51ea67b
Albert <albertweb08@gmail.com>
2023-11-13 19:12:24 +0100

    Menu prompt

--- diccionarioSinonimos.js
+++ diccionarioSinonimos.js
@@ -1,1 +1,32 @@
 // ALUMNO: ALBERT PÉREZ BALEYTO
+
+const diccionario = new DiccionarioDeSinonimos();
+while (true) {
+const opcion = prompt("Selecciona una opción:\n1. Agregar sinónimo\n2. Obtener sinónimos\n3. Eliminarsinónimo\n4. Eliminar palabra\n5. Salir");
+switch (opcion) {
+case "1":
+const palabra = prompt("Ingresa una palabra:");
+const sinonimo = prompt("Ingresa un sinónimo:");
+diccionario.agregarSinonimo(palabra, sinonimo);
+break;
+case "2":
+const palabraConsultar = prompt("Ingresa una palabra a consultar:");
+const sinonimos = diccionario.obtenerSinonimos(palabraConsultar);
+console.log(sinonimos);
+break;
+case "3":
+const palabraEliminarSinonimo = prompt("Ingresa una palabra:");
+const sinonimoAEliminar = prompt("Ingresa el sinónimo a eliminar:");
+diccionario.eliminarSinonimo(palabraEliminarSinonimo, sinonimoAEliminar);
+break;
+case "4":
+const palabraEliminar = prompt("Ingresa una palabra a eliminar:");
+diccionario.eliminarPalabra(palabraEliminar);
+break;
+case "5":
+alert("Saliendo del programa");
+return;
+default:
+alert("Opción no válida. Por favor, elige una opción válida.");
+}
+}
